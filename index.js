710df0a975de63e6c873acf6decc9e797aa1a879
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-04-20 22:57:00 +0530

    added view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //use express router
 app.use('/', require('./routes'));
 
+//set up view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');//contact list yhi line __dirname krke kia the bs usi ko aise v likh skte h
+
 app.listen(port, function(err){
     if(err){console.log(`Error in running the server, ${err}`)};
     console.log(`Server is running on port: ${port}`);
