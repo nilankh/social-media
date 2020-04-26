d5240ab7e298198c82dc7af0d3a3d2946f6d1e5c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-04-26 16:43:58 +0530

    add saving posts to the db

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,6 @@ router.use('/users', require('./users'));
 //for any  further routes, acces from here
 //router.use('/routerName', require('./routerFile'));
 
+// ye line jo likh rhe h wo schema create krn ke baad databas wala module me h
+router.use('/posts', require('./posts'));
 module.exports = router;
