cee946ffce0b2846f2c4fb129e1a0db05f7e2c06
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-04-27 20:32:04 +0530

    add distrubuting the code into partials

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');//these line islia import kia q ki hme comment v deleete krna h q ki jb post delete hoga tb b toa comment v jhyga na
 
 module.exports.create = function(req, res){
     Post.create({
@@ -10,5 +11,19 @@ module.exports.create = function(req, res){
 
         return res.redirect('back');
 
-    })
+    });
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        //.id means converting the object id into string
+        if(post.user == req.user.id){//post == user the user who is deleting a post, who is written the post  
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
