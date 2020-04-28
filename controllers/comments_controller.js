8b966a052097740440970070a50437f1c0c076c0
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-04-28 17:11:50 +0530

    converted comment's controller into async await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,27 +1,30 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
+module.exports.create = async function(req, res){
     
-    //we need to create comment over post, we need to find whether that post exist
+    try{
+        //we need to create comment over post, we need to find whether that post exist
 
-    Post.findById(req.body.post, function(err, post){
-        if(post){//if post is found
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err, comment){
-                //handle error
-
-                //adding comment to push
-                post.comments.push(comment);
-                post.save();//whenever i m updating something we have to save so that we can block, or bola jye save kr lia db me
-
-                res.redirect('back');
-            });
-        }
-    });
+    let post = await Post.findById(req.body.post);
+        
+    if(post){//if post is found
+        let comment = await Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+        });
+        //adding comment to push
+        post.comments.push(comment);
+        post.save();//whenever i m updating something we have to save so that we can block, or bola jye save kr lia db me
+        res.redirect('/');
+    }
+    
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
 }
 
 
