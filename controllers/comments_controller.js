3804d8ed8f1e6b7110957ad0b61e93b3a0ee31c2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-04-28 17:19:52 +0530

    fully converted comments controller into async await function

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -28,16 +28,23 @@ module.exports.create = async function(req, res){
 }
 
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
+module.exports.destroy = async function(req, res){
+    try{
+        let comment = await Comment.findById(req.params.id);
+        
         if(comment.user == req.user.id){
             let postId = comment.post;
+                
             comment.remove();
-            Post.findByIdAndUpdate(postId, { $pull :{comments:req.params.id}}, function(err, post){
+            let post = Post.findByIdAndUpdate(postId, { $pull :{comments:req.params.id}});
+                    return res.redirect('back');
+                
+            }else{
                 return res.redirect('back');
-            })
-        }else{
-            return res.redirect('back');
-        }
-    });
+            }
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+ 
 }
