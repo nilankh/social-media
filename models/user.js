ab925e8c884e7eeaf6d5174486560dff90706238
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-04-30 16:20:01 +0530

    installed multer and set it to models user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+// importing multer y we are not uploading multer into config folder but in user page, reason bcz we are uploading the files specific to user and we have some specific setting users avatar willl be uploaded at different place for ex if u posting that will be uplaoded at different place, posting an image,so we are setting up multer individually for each model all the you can centralised approach of it..but for now lets go with this
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
 const userScehma = new mongoose.Schema({
     email: {
         type: String,
