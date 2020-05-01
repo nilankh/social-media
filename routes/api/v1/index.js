518e7c8d72a3d35639b806dd1df9e87c909961ce
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-01 22:26:41 +0530

    rendering on api step2

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,1 +1,10 @@
 // this the route index for v1 routes
+
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/posts', require('./posts'));
+
+
+module.exports = router;
